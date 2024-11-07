06589d0919366bfc956d089e29c729ecd77d0bdc
AFB <mxsh741@gmail.com>
2024-11-07 22:46:16 +0500

    feat: функция рассчёта стоимости

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+interface TotalPriceParams {
+  price: number;
+  discount: number;
+  isInstallment: boolean;
+  months: number;
+}
+
+const totalPrice = ({
+  price,
+  discount,
+  isInstallment,
+  months,
+}: TotalPriceParams): number => {
+  if (discount > 0) {
+    return isInstallment
+      ? (price - (price / 100) * discount) / months
+      : price - (price / 100) * discount;
+  }
+  return isInstallment ? price / months : price;
+};
+
+console.log(
+  totalPrice({ price: 100000, discount: 25, isInstallment: true, months: 12 })
+);
